6661e7d58d2583dfd7442ff965aa20ee6dbce9bc
taesikyoo <yrns0907@gmail.com>
2020-10-07 00:02:16 +0900

    [실습2] MyArrayList

--- intellij/code/src/main/java/com/allendowney/thinkdast/MyArrayList.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/MyArrayList.java
@@ -45,7 +45,16 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public boolean add(T element) {
 		// TODO: FILL THIS IN!
-		return false;
+		if (size >= array.length) {
+			// make a bigger array and copy over the elements
+			@SuppressWarnings("unchecked")
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
 	}
 
 	@Override
@@ -111,6 +120,9 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: FILL THIS IN!
+		for (int i = 0; i < size; i++) {
+			if (equals(target, get(i))) return i;
+		}
 		return -1;
 	}
 
@@ -182,7 +194,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T remove(int index) {
 		// TODO: FILL THIS IN!
-		return null;
+		T element = get(index);
+		for (int i=index; i<size-1; i++) {
+			array[i] = array[i+1];
+		}
+		size--;
+		return element;
 	}
 
 	@Override
@@ -202,7 +219,9 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T set(int index, T element) {
 		// TODO: FILL THIS IN!
-		return null;
+		T temp = get(index);
+		array[index] = element;
+		return temp;
 	}
 
 	@Override
