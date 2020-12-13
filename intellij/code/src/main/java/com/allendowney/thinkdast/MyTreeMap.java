e175248fcbeaa040e41d54380236e73849226d71
taesikyoo <yrns0907@gmail.com>
2020-12-13 23:25:06 +0900

    [실습10] MyTreeMap

--- intellij/code/src/main/java/com/allendowney/thinkdast/MyTreeMap.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/MyTreeMap.java
@@ -72,6 +72,13 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 		Comparable<? super K> k = (Comparable<? super K>) target;
 
 		// TODO: FILL THIS IN!
+		Node node = root;
+		while (node != null) {
+			int cmp = k.compareTo(node.key);
+			if (cmp < 0) node = node.left;
+			else if (cmp > 0) node = node.right;
+			else return node;
+		}
 		return null;
 	}
 
@@ -96,6 +103,10 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	private boolean containsValueHelper(Node node, Object target) {
 		// TODO: FILL THIS IN!
+		if (node == null) return false;
+		if (equals(target, node.value)) return true;
+		if (containsValueHelper(node.left, target)) return true;
+		if (containsValueHelper(node.right, target)) return true;
 		return false;
 	}
 
@@ -122,7 +133,15 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	public Set<K> keySet() {
 		Set<K> set = new LinkedHashSet<K>();
 		// TODO: FILL THIS IN!
+		addInOrder(root, set);
 		return set;
+	}
+
+	private void addInOrder(Node node, Set<K> set) {
+		if (node == null) return;
+		addInOrder(node.left, set);
+		set.add(node.key);
+		addInOrder(node.right, set);
 	}
 
 	@Override
@@ -140,7 +159,32 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	private V putHelper(Node node, K key, V value) {
 		// TODO: FILL THIS IN!
-		return null;
+		@SuppressWarnings("unchecked")
+		Comparable<? super K> k = (Comparable<? super K>) key;
+		int cmp = k.compareTo(node.key);
+
+		if (cmp < 0) {
+			if (node.left == null) {
+				node.left = new Node(key, value);
+				size++;
+				return null;
+			} else {
+				return putHelper(node.left, key, value);
+			}
+		}
+		if (cmp > 0) {
+			if (node.right == null) {
+				node.right = new Node(key, value);
+				size++;
+				return null;
+			} else {
+				return putHelper(node.right, key, value);
+			}
+		}
+
+		V oldValue = node.value;
+		node.value = value;
+		return oldValue;
 	}
 
 	@Override
